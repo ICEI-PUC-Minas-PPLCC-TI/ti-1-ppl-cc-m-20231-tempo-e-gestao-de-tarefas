e215cdfb6687f5bbd00f7fd652b6bedbf0c9788a
Rafael Peixoto <rafaelolpeixoto@gmail.com>
2023-07-03 21:50:50 -0300

    navegação para tela de edicao de rotinas

--- codigo/Pagina_Inicial/js/script.js
+++ codigo/Pagina_Inicial/js/script.js
@@ -13,3 +13,8 @@ $(".slide div").on("click" , function(){
 $(this).toggleClass('zoomed');
 $(".overlay").toggleClass('active');
 })
+
+function edicao_rotina(id){
+    let url = "../Edicao_de_rotinas/index.html?id="+id;
+    window.location.href = url;
+}
